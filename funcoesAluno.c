9d746ab2dadb092e342dab95441989e643a3d2fb
Marcus Vinícius Campos Fonseca <marcus.080196@gmail.com>
2014-10-28 11:38:24 -0200

    Update funcoesAluno.c

--- funcoesAluno.c
+++ funcoesAluno.c
@@ -1,17 +1,17 @@
-// Funções relacionadas aos dados de alunos
+// FunÃ§Ãµes relacionadas aos dados de alunos
 
 #include <stdio.h>
 #include "dados.h"
 
 //***********************************************************************************************************************
 //  Objetivo: Pesquisar um aluno dentro de um arquivo por matricula
-//  Parâmetros: matricula a ser pesquisada, indicador se o dado encontrado deve ser escrito (não zero para sim)
+//  ParÃ¢metros: matricula a ser pesquisada, indicador se o dado encontrado deve ser escrito (nÃ£o zero para sim)
 //  Retorno: numero positivo se encontrado (posicao do aluno no arquivo de 1 a n, sendo n o numero de alunos), 0 - codigo nao encontrado
 int pesquisaAlunoMatricula(int matriculaBusca, int indPrint)
 {
     FILE *arq;
     Aluno aluno;
-    int pos = 0;
+    int pos = 0, flag = 0;
     if((arq = fopen(ARQ_CURSOS, "rb")) != NULL)
     {
         while(!feof(arq))
@@ -23,10 +23,13 @@ int pesquisaAlunoMatricula(int matriculaBusca, int indPrint)
                     if(indPrint)
                         // TODO Formatar dados saida aluno
                         printf("");
+                    flag = 1;
                     break;
                 }
             }
         fclose(arq);
     }
+    if(indPrint && !flag)
+        printf("O aluno nao foi encontrado!");
     return pos;
 }
