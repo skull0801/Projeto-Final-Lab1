2343d9e6518a07d41c0b0d2f65b60093e6ab18ef
lucask84ever <fox_krb@yahoo.com.br>
2014-10-29 12:18:08 -0200

    Update dados.h

--- dados.h
+++ dados.h
@@ -2,19 +2,19 @@
 #define DADOS_H
 #include <time.h>
 
-// Definições
+// DefiniÃ§Ãµes
 #define TAM_NOME_ALUNO 81
 #define TAM_NOME_CURSO 51
 #define TAM_CPF 12
 #define CODIGO_MIN 1001
 #define CODIGO_MAX 99999
-#define CARGA_HORARIA_MIN 20
+#define CARGA_HORARIA_MIN 30
 #define CARGA_HORARIA_MAX 1000
-#define MENSALIDADE_MIN 100
-#define MENSALIDADE_MAX 100000
-#define ARQ_ALUNOS "alunos.txt"
-#define ARQ_CURSOS "cursos.txt"
-#define ARQ_MATRICULAS "mat.txt"
+#define MENSALIDADE_MIN 250.0
+#define MENSALIDADE_MAX 10000.0
+#define ARQ_ALUNOS "alunos.b"
+#define ARQ_CURSOS "cursos.b"
+#define ARQ_MATRICULAS "matriculas.b"
 #define MENU_PRINCIPAL "menu.txt"
 #define MENU_BUSCA "subMenuBusca.txt"
 #define MENU_CADASTRO "subMenuCadastro.txt"
@@ -44,25 +44,24 @@ typedef struct
     char situacaoAluno, situacaoPagamento;
 }Cadastro;
 
-/*  Funções
-//  Protótipos
+//   FunÃ§Ãµes
+//  ProtÃ³tipos
 int leValidaInteiro(const char *msg, const char *identificacao, int valorMin, int valorMax);
 float leValidaReal(const char *msg, const char *identificacao, float valorMin, float valorMax);
 void leValidaTexto(char *texto, const char *msg, const char *identificacao, int valorMin, int valorMax);
 char leValidaChar(const char *titulo, const char *escolhas);
+void strToLower(char *string);
+void apresentaMenu(char *nomeArquivo);
 
 void leDadosCurso(Curso *curso);
 void gravaDadosCurso(Curso *curso);
 void listaDadosCurso();
 int pesquisaCursoCod(int codCursoBusca, int indPrint);
 void pesquisaCursoNome(char *nomeBusca);
+int achaProximoCodCurso();
+void ordenaCursosPeloNome(Curso *cursos, int qtdeCursos);
 
-void apresentaMenu(char *nomeArquivo);
-
-// Funcionais
-void strToLower(char *string);
-
-// Ainda nao serao usadas
+/* Ainda nao serao usadas
 
 int leDadosAluno(Aluno *aluno);
 void gravaDadosAluno(Aluno *aluno);
@@ -75,5 +74,4 @@ void pesquisaCadastro(Cadastro *query, int paramBusca);
 void gravaDadosCadastro(Cadastro *cadastro);
 void listaDadosCadastro();
 */
-
 #endif
