f911ecf3b6acb365bc872e71e1d94d153bc745af
Marcus Vinícius Campos Fonseca <marcus.080196@gmail.com>
2014-10-29 15:38:40 -0200

    Update main.c
    Novos menus e opções

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
-// Alunos : Marcus Vinícius Campos Fonseca e Lucas Lima da Silva
-// Matrículas: UC14101868, UC14102446
-// Implementação de algumas das funcionalidades do projeto final
+// Alunos : Marcus VinÃ­cius Campos Fonseca e Lucas Lima da Silva
+// MatrÃ­culas: UC14101868, UC14102446
+// ImplementaÃ§Ã£o de algumas das funcionalidades do projeto final
 
 #include <stdio.h>
 #include <conio.c>
@@ -10,6 +10,7 @@ int main(void)
 {
 // Declaracoes
     Curso curso;
+    Aluno aluno;
     int posCursoEncontrado;
     char opcao, subOpcao, nomeBusca[100];
     int cod;
@@ -17,7 +18,7 @@ int main(void)
     do
     {
         apresentaMenu(MENU_PRINCIPAL);
-        opcao = leValidaChar("", "1230");
+        opcao = leValidaChar("", "123450");
 	    switch(opcao)
 	    {
             case '1':
@@ -26,7 +27,8 @@ int main(void)
             	switch(subOpcao)
             	{
             		case '1':
-                        puts("Ainda nao implementado!");
+                        leDadosAluno(&aluno);
+                        gravaDadosAluno(&aluno);
                         getch();
             			break;
             		case '2':
@@ -41,22 +43,62 @@ int main(void)
                 }	
 	            break;
             case '2':
+                apresentaMenu(MENU_ALTERAR);
+                subOpcao = leValidaChar("", "1230");
+            	switch(subOpcao)
+            	{
+            		case '1':
+            			cod = leValidaInteiro("Informe a matricula a buscar", "Matricula", MATRICULA_MIN, MATRICULA_MAX);
+            			alteraDadosAluno(cod);
+                        getch();
+            			break;
+            		case '2':
+            			puts("Ainda nao implementado!");
+                        getch();
+            			break;
+            		case '3':
+            			puts("Ainda nao implementado!");
+                        getch();
+            			break;	
+            	}
+            	break;
+            case '3':
+                apresentaMenu(MENU_EXCLUIR);
+                subOpcao = leValidaChar("", "1230");
+            	switch(subOpcao)
+            	{
+            		case '1':
+            			puts("Ainda nao implementado!");
+                        getch();
+            			break;
+            		case '2':
+            			puts("Ainda nao implementado!");
+                        getch();
+            			break;
+            		case '3':
+            			puts("Ainda nao implementado!");
+                        getch();
+            			break;	
+            	}
+            	break;
+            case '4':
                 apresentaMenu(MENU_BUSCA);
                 subOpcao = leValidaChar("", "12340");
 				switch(subOpcao)
 				{
 					case '1':
-            			puts("Ainda nao implementado!");
+            			cod = leValidaInteiro("Informe a matricula a buscar", "Matricula", MATRICULA_MIN, MATRICULA_MAX);
+            			pesquisaAlunoMatricula(cod, 1);
                         getch();
             			break;
             		case '2':
-            			cod = leValidaInteiro("Informe o codigo buscado", "Codigo", CODIGO_MIN, CODIGO_MAX);
+            			cod = leValidaInteiro("Informe o codigo a buscar", "Codigo", CODIGO_MIN, CODIGO_MAX);
             			pesquisaCursoCod(cod, 1);
             			getch();
             			break;
             		case '3':
 						leValidaTexto(nomeBusca, "Informe o nome do curso", "Nome", 1, TAM_NOME_CURSO);
-						pesquisaCursoNome(nomeBusca);  // Mesmo não sendo necessario à solução atual,implementamos o mesmo para prática
+						pesquisaCursoNome(nomeBusca);
 						getch();
 						break;
             		case '4':
@@ -64,14 +106,15 @@ int main(void)
                         getch();
             			break;
 				}
+				
                 break;
-            case '3':
+            case '5':
                 apresentaMenu(MENU_LISTA);
                 subOpcao = leValidaChar("", "1230");
             	switch(subOpcao)
             	{
             		case '1':
-            			puts("Ainda nao implementado!");
+                        listaDadosAlunos();
                         getch();
             			break;
             		case '2':
@@ -83,6 +126,7 @@ int main(void)
                         getch();
             			break;	
             	}
+            	break;            	
 	      }
  }
  while(opcao != '0');
