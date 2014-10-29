b4049ceda2f17e2ce4c9bd5af1e3307efebfa28c
Marcus Vinícius Campos Fonseca <marcus.080196@gmail.com>
2014-10-29 15:36:38 -0200

    Novos Protótipos e Nova Estrutura
    Agora a estrutura de aluno tem uma estrtura de data criada pelo programa)

--- dados.h
+++ dados.h
@@ -8,25 +8,37 @@
 #define TAM_CPF 12
 #define CODIGO_MIN 1001
 #define CODIGO_MAX 99999
+#define MATRICULA_MIN 1
+#define MATRICULA_MAX 9999999
 #define CARGA_HORARIA_MIN 30
 #define CARGA_HORARIA_MAX 1000
 #define MENSALIDADE_MIN 250.0
 #define MENSALIDADE_MAX 10000.0
+#define MIN_IDADE 15
+#define MAX_IDADE 150
 #define ARQ_ALUNOS "alunos.b"
 #define ARQ_CURSOS "cursos.b"
 #define ARQ_MATRICULAS "matriculas.b"
 #define MENU_PRINCIPAL "menu.txt"
 #define MENU_BUSCA "subMenuBusca.txt"
 #define MENU_CADASTRO "subMenuCadastro.txt"
+#define MENU_ALTERAR "subMenuAlterar.txt"
+#define MENU_EXCLUIR "subMenuExcluir.txt"
 #define MENU_LISTA "subMenuLista.txt"
 
 // Estruturas
+// Data
+typedef struct
+{
+    int dia, mes, ano;
+}Data;
+
 // Aluno
 typedef struct
 {
     char nome[TAM_NOME_ALUNO], cpf[TAM_CPF], sexo;
     int idade, matricula;
-    struct tm dataIngresso;
+    Data dataIngresso;
 }Aluno;
 
 // Curso
@@ -52,6 +64,7 @@ void leValidaTexto(char *texto, const char *msg, const char *identificacao, int 
 char leValidaChar(const char *titulo, const char *escolhas);
 void strToLower(char *string);
 void apresentaMenu(char *nomeArquivo);
+void geraDataIngresso(Data *data);
 
 void leDadosCurso(Curso *curso);
 void gravaDadosCurso(Curso *curso);
@@ -61,13 +74,18 @@ void pesquisaCursoNome(char *nomeBusca);
 int achaProximoCodCurso();
 void ordenaCursosPeloNome(Curso *cursos, int qtdeCursos);
 
-/* Ainda nao serao usadas
-
-int leDadosAluno(Aluno *aluno);
+void leDadosAluno(Aluno *aluno);
 void gravaDadosAluno(Aluno *aluno);
 void listaDadosAlunos();
-void pesquisaAlunoNome(char *nomeBusca);
 int pesquisaAlunoMatricula(int matriculaBusca, int indPrint);
+// void pesquisaAlunoNome(char *nomeBusca);
+void alteraDadosAluno(int matricula);
+void excluiDadosAluno();
+int validaCPF(const char *cpf);
+int verificaCPFAluno(const char *cpf);
+
+
+/* Ainda nao serao usadas
 
 int leDadosCadastro(Cadastro *cadastro);
 void pesquisaCadastro(Cadastro *query, int paramBusca);
