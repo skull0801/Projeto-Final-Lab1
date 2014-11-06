98345d19ef24fc8ef91df1f043c5dd4eeaec8bf1
Marcus Vinícius Campos Fonseca <marcus.080196@gmail.com>
2014-11-06 16:10:02 -0200

    Update funcoesCadastro.h

--- funcoesCadastro.h
+++ funcoesCadastro.h
@@ -8,6 +8,11 @@ void listaDadosCadastro();
 void gravaDadosMatricula(Cadastro matricula);
 void apresentaAlunosMatriculadosEmUmCurso();
 int verificaAlunoRepetidoCadastrando(int codCurso, int alunoMatricula);
-void apresentaAlunosMatriculadosEmUmCurso(int codCurso, int indicador);
+void apresentaAlunosMatriculadosEmUmCurso();
+void excluiMatriculaAluno(int matricula, int codCurso);
+void apresentarTodosAlunosCadastrados();
+void alteraDadosCadastro();
+int obtemCadastroArquivo(Cadastro *matricula, int posCadastro);
+Aluno *obtemAlunosDeCurso(int codigoCurso, int *qtdAlunos, int indicador);
 
 #endif
