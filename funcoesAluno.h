5d65844e8a4ce916a1a901da85c7ef9b3f2567e2
Marcus Vinícius Campos Fonseca <marcus.080196@gmail.com>
2014-11-07 13:30:36 -0200

    Reorganizacao dos Headers

--- funcoesAluno.h
+++ funcoesAluno.h
@@ -3,27 +3,39 @@
 #define FUNCOESALUNO_H
 #include "dados.h"
 
+// Cadastro
 void cadastraAluno(void);
 void leDadosAluno(Aluno *aluno);
 void gravaDadosAluno(Aluno *aluno);
-void listaDadosAlunos(void);
-void apresentaAluno(Aluno aluno);
-void pesquisaAluno(void);
-int apresentaTodosAlunos(void);
-int apresentaDadosAlunos(Aluno *alunos, int qtdAlunos);
-Aluno * obtemDadosAlunosArquivo(int * qtdAlunos);
-void ordenaAlunosPorNome(Aluno *alunos, int qtdAlunos);
-void pesquisaApresentaAlunoNome();
-void pesquisaApresentaAlunoMatricula();
-int pesquisaAlunoMatricula(int matriculaBusca);
-void pesquisaCurso(void);
-// void pesquisaAlunoNome(char *nomeBusca);
+
+// Alteracao
 void alteraAluno(void);
 void alteraDadosAluno(Aluno aluno, int posAluno);
+
+// Exclusao
 void excluiAluno(void);
 void excluiDadosAluno(int posAluno);
+
+// Apresentacao
+void apresentaAluno(Aluno aluno);
+int apresentaTodosAlunos(void);
+int apresentaDadosAlunos(Aluno *alunos, int qtdAlunos);
+
+// Pesquisa
+void pesquisaAluno(void);
+int pesquisaAlunoMatricula(int matriculaBusca);
+void pesquisaApresentaAlunoNome();
+void pesquisaApresentaAlunoMatricula();
+
+// Obtencao do arquivo
+Aluno * obtemDadosAlunosArquivo(int * qtdAlunos);
+int obtemAlunoArquivo(Aluno *aluno, int posAluno);
+
+// CPF
 int validaCPF(const char *cpf);
 int verificaCPFAluno(const char *cpf);
-int obtemAlunoArquivo(Aluno *aluno, int posAluno);
+
+// Comparacao
+int comparaAlunos(const void *p1, const void *p2);
 
 #endif
